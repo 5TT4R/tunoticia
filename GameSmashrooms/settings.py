497c00d70400715efcad8b3a884a70c08b0e37f8
5TT4R <TO.ARRIAGADAV@DUOCUC.CL>
2024-06-24 03:53:06 -0400

    1.8

--- GameSmashrooms/settings.py
+++ GameSmashrooms/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o462$wx9gi^_r)mw^&-67rie3n#mynrtw+!+aiw8a+!p%g&502'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 
@@ -193,7 +193,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS= [BASE_DIR/'static']
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
