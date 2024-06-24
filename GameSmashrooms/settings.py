cd521c29a1d452a2093f6e7b74b6246f1b4aa30b
5TT4R <TO.ARRIAGADAV@DUOCUC.CL>
2024-06-24 03:44:36 -0400

    1.5

--- GameSmashrooms/settings.py
+++ GameSmashrooms/settings.py
@@ -193,10 +193,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILE_DIRS= [BASE_DIR/'static']
+STATIC_URL = '/static/'
+STATICFILE_DIRS= [BASE_DIR/'/static/']
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / '/staticfiles/'
 
 
 
